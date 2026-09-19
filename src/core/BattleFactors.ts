ace32e4429d18d49967fd44ea3103fe9be4ef536
Rohan Panwar <tusharpanwar469@gmail.com>
2026-09-19 21:29:13 +0530

    feat: add battle factors model

---
+++ src/core/BattleFactors.ts
@@ -0,0 +1,14 @@
+export interface BattleFactors {
+  power: number;
+  durability: number;
+  speed: number;
+  intelligence: number;
+  combatSkill: number;
+  abilities: number;
+  equipment: number;
+  battlefield: number;
+  endurance: number;
+  teamwork: number;
+  magic: number;
+  technology: number;
+}
